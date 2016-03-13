343b089ed5a284692435cc37e7238788d371f4e8
gao_st@126.com <gao_st@126.com>
2016-03-13 10:00:10 +0800

    placeholder插件优化

--- jQuery插件/placeholder/jquery.placeholder.js
+++ jQuery插件/placeholder/jquery.placeholder.js
@@ -18,27 +18,27 @@
     		return;
     	}
     	
-    	$('body')
-	    	.on('click', '.wbc-placeholder', function() {
-	    		$(this).prev().focus();
-	    	})
-	    	.on('keyup', 'input', function() {
-    			var $this = $(this), 
-    				val = this.value;
-    			if (val.length > 0) {
-    				$this.next('.wbc-placeholder').hide();
-    			} else {
-    				$this.next('.wbc-placeholder').show();
-    			}
-	    	});
-    	
         this.each(function() {
             var $this = $(this),
             	txt = $this.attr('placeholder');
             	
             $this.wrap('<span class="input"></span>');
-            $this.after('<span class="wbc-placeholder">' + txt + '</span>');
-        });
+            $this
+            	.on('keyup', function() {
+	    			var $this = $(this), 
+	    				val = this.value;
+	    			if (val.length > 0) {
+	    				$this.next('.wbc-placeholder').hide();
+	    			} else {
+	    				$this.next('.wbc-placeholder').show();
+	    			}
+		    	})
+            	.after('<span class="wbc-placeholder">' + txt + '</span>')
+            	.next('.wbc-placeholder')
+            	.on('click', function() {
+            		$(this).prev().focus();
+            	});
+        });        
     };
     
     $('input').placeholder();
