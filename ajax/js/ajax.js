14cb25f806655a115e0837fe4d786ffcf1800ff7
gao_st@126.com <gao_st@126.com>
2016-03-13 16:44:15 +0800

    级联菜单一

--- ajax/js/ajax.js
+++ ajax/js/ajax.js
@@ -1,10 +1,30 @@
-var url = 'http://dohtml5.duapp.com/php/wbc2/data.php?callback=?',
+/*var url = 'http://dohtml5.duapp.com/php/wbc2/data.php?callback=?',
 	param = {username: 'situ', age: 30};
 
 $.get(url, param, onSuccess, 'json');
 
 function onSuccess(data) {
 	console.log(data.age);
-}
+}*/
 
 //console.log(data);
+
+// jsonP
+
+!function(window, document, $, undefined) {
+	
+	var renderSel = function() {
+		var url = 'http://dohtml5.duapp.com/php/wbc2/getPlace.php?callback=?';
+		$.get(url, function(data) {
+			var arr = [];
+			$.each(data, function(i, obj) {
+				arr.push('<option value="', obj.id, '">', obj.province, '</option>');
+			});
+			
+			$('#provinceSel').html(arr.join(''));
+		}, 'json');
+	};
+	
+	renderSel();
+	
+}(window, document, jQuery);
