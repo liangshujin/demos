26e8ec1b2f2ee71cac9c3dcad5910f10955a2cd0
situ <gao_st@126.com>
2016-04-24 21:43:49 +0800

    reset

--- 随机任务（数组随机排序）/js/order.js
+++ 随机任务（数组随机排序）/js/order.js
@@ -3,19 +3,15 @@
  */
 !function(window, document, $, undefined) {
 
-    var students = '阚鑫磊 魏先强 李晶 时刘韩 王珂 郇昌 杨天骄 詹时雨 张勃 张金洋 周振洋'.split(' '),
+    var students = '魏先强 窦子龙 宋韶华 张亚娟 宋晓彦 郭永强 王乔熙'.split(' '),
         urls = [
-            'http://bj.jumei.com/(聚美优品)',
-            'http://caipiao.163.com/(网易彩票)',
-            'http://baoxian.163.com/(网易保险)',
-            'http://piao.163.com/(网易电影票)',
-            'http://mall.163.com/(网易商城)',
-            'http://www1.fang.com/(搜房网)',
-            'https://ju.taobao.com/(聚划算)',
-            'http://www.autohome.com.cn/(汽车之家)',
-            'http://beijing.bitauto.com/(易车网)',
-            'http://www.gome.com.cn/(国美在线)',
-            'http://www.yhd.com/(一号店)'
+            'http://www.mia.com/(密芽)',
+            'http://www.mogujie.com/(蘑菇街)',
+            'http://www.zhe800.com/(折800)',
+            'http://www.mafengwo.cn/(马蜂窝)',
+            'http://www.haibao.com/(海报时尚网)',
+            'http://y.qq.com/#type=index(qq音乐)',
+            'http://beijing.haodai.com/(好贷网)'
         ],
         timer;
 
