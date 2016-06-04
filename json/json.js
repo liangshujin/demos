d20c0d5481368704f30380103fe50da1d5eb2ed6
wbc <wbc201605@sina.com>
2016-06-04 16:18:48 +0800

    wbc4 用户管理1

--- json/json.js
+++ json/json.js
@@ -1,13 +1,13 @@
 // 值的有序列表，数组
-[]
+// []
 
-['a']
+// ['a']
 
-var str = "['a', 'b', 3, true, null, undefined, {\"name\": \"张三\"}, ['a']]";
+// var str = "['a', 'b', 3, true, null, undefined, {\"name\": \"张三\"}, ['a']]";
 
-str = ['a', 'b', 3, true, null, undefined, {name: "张三"}, ['a']];
+// str = ['a', 'b', 3, true, null, undefined, {name: "张三"}, ['a']];
 
-console.log(str[6]);
+// console.log(str[6]);
 
 /*
  * json对象
@@ -15,34 +15,59 @@ console.log(str[6]);
  */
 
 // 键值对的集合，对象
-{}
+// {}
 
-{age: 20}
+// {age: 20}
 
-{
-	age: 20, 
-	name: '张三', 
-	firends: ['李四', '王五'], 
-	wife: null, 
-	computer: {
-		cpu: 'i5', 
-		memory: '4g'
-	}
-}
+// var p = {
+// 	age: 20, 
+// 	name: '张三', 
+// 	firends: ['李四', '王五'], 
+// 	wife: null, 
+// 	computer: {
+// 		cpu: 'i5', 
+// 		memory: '4g'
+// 	}
+// }
 
 /*
  * 
  */
 
-var company = {
-	name: "百度",
-	boss: "李彦宏",
-	address: {
-		"address1": "上地五街",
-		"address2": "上地九街",
-		"address3": "上地十街",
-		"address4": "软件园2期（后厂村路）"
-	},
-	stuff_num: 200000
-	// ...
-};
+// var company = {
+// 	name: "百度",
+// 	boss: "李彦宏",
+// 	address: {
+// 		"address1": "上地五街",
+// 		"address2": "上地九街",
+// 		"address3": "上地十街",
+// 		"address4": "软件园2期（后厂村路）"
+// 	},
+// 	stuff_num: 200000
+// };
+
+// console.log(company.address.address1)
+
+// var obj = {
+// 	name: 'aaa',
+// 	age: 20,
+// 	say: function() {
+// 		alert(this.age);
+// 	}
+// };
+
+// gson.jar    fastjson.jar
+
+var p = '{"age":20,"name":"张三","firends":["李四","王五"],"wife":null,"computer":{"cpu":"i5","memory":"4g"}}';
+
+// p = eval('(' + p + ')');
+
+p = JSON.parse(p);
+
+console.log(p);
+
+p = JSON.stringify(p);
+
+console.log(p);
+
+console.log(typeof p)
